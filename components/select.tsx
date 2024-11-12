0fd262d6738f3873438eef1d45f4187076037ef9
batir <github.prenatal113@passinbox.com>
2024-11-12 11:06:44 +0100

    done select component

--- components/select.tsx
+++ components/select.tsx
@@ -1,27 +1,36 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { ChevronDown } from "lucide-react";
+import { ChangeEventHandler } from "react";
 
 interface Props {
   options: { [value: string]: string };
   defaultValue?: string;
   className?: string;
+  onChange?: ChangeEventHandler<HTMLSelectElement>;
 }
 
-export default function Select({ options, defaultValue, className }: Props) {
+export default function Select({
+  options,
+  defaultValue,
+  className,
+  onChange,
+}: Props) {
   return (
-    <select
-      defaultValue={defaultValue}
-      className={cn(
-        "hover:bg-gray-100 outline-none appearance-none px-8",
-        className,
-      )}
-    >
-      {Object.entries(options).map(([value, content], index) => (
-        <option key={index} value={value}>
-          {content}
-        </option>
-      ))}
-    </select>
+    <div className={cn("relative", className)}>
+      <select
+        onChange={onChange}
+        defaultValue={defaultValue}
+        className="outline-none appearance-none hover:bg-gray-100 p-1 pr-6  rounded transition"
+      >
+        {Object.entries(options).map(([value, content], index) => (
+          <option key={index} value={value}>
+            {content}
+          </option>
+        ))}
+      </select>
+      <ChevronDown className="absolute w-4 h-4 right-1 top-1/2 -translate-y-1/2 pointer-events-none" />
+    </div>
   );
 }
